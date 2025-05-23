fffd993520152aec3be10dd73ff5f464e5b76dd6
Johan Melin <soldierheart@hotmail.com>
2025-05-23 07:17:36 +0200

    feat(index): add contact card with search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { View, FlatList } from 'react-native';
+import { useHeaderSearchBar } from '~/lib/useHeaderSearchBar';
+import { ContactCard } from '~/components/ContactCard';
+
+export interface Contact {
+  id: string;
+  name: string;
+}
 
 export default function TabOne() {
+  const contacts: Contact[] = [{ id: "1", name: 'Adam' }, { id: "2", name: 'Eve' }];
+  const searchValue = useHeaderSearchBar({ hideWhenScrolling: contacts.length === 0 });
+
+  const data = searchValue
+    ? contacts.filter((c) => c.name.toLowerCase().includes(searchValue.toLowerCase()))
+    : contacts;
+
   return (
-    <View>
-      <Text>Tab One</Text>
+    <View className="flex-1 p-4">
+      <FlatList 
+      data={data} 
+      renderItem={({ item }) => <ContactCard contact={item} />}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => <View className="h-4" />}
+      />
     </View>
   );
 }
---
+++ components/ContactCard.tsx
@@ -0,0 +1,30 @@
+import { View, Pressable } from "react-native";
+import { Text } from "~/components/nativewindui/Text";
+import { Avatar, AvatarFallback } from "./nativewindui/Avatar";
+
+export const ContactCard = ({ contact, ...props }: { contact: any }) => {
+  return (
+    <Pressable {...props} style={{elevation: 8 }} className="shadow-sm rounded-xl p-4 bg-card flex-row items-center">
+      <Avatar alt={contact.name}>
+        <AvatarFallback className="bg-blue-500">
+          <Text className="text-2xl text-white">{contact.name.charAt(0).toUpperCase()}</Text>
+        </AvatarFallback>
+      </Avatar>
+      <View className="ml-4">
+        <Text className="font-bold">{contact.name}</Text>
+        <Text>
+          {contact.lastContact
+            ? "Last Contact: " + new Date(contact.lastContact).toLocaleDateString('en-GB', {
+                day: 'numeric',
+                month: 'short',
+                year: 'numeric'
+              })
+            : ""}
+        </Text>
+        {contact.shouldContact && (
+          <Text className="text-red-500">Should Contact</Text>
+        )}
+      </View>
+    </Pressable>
+  );
+};
