f9cd7eb80f2378f9f9a24d41ff115ced677841c0
Johan Melin <soldierheart@hotmail.com>
2025-05-30 13:32:35 +0200

    styling(setContact): update text input style for consistency

--- app/setContact.tsx
+++ app/setContact.tsx
@@ -102,7 +102,7 @@ export default function Modal() {
           <Text variant="heading" className="mb-2">Name</Text>
           <TextInput
             ref={nameInputRef}
-            className="border border-gray-300 px-4 py-2 mb-2 w-full text-lg rounded text-foreground"
+            className={"border border-border bg-card px-4 py-2 mb-2 w-full text-lg rounded text-foreground"}
             placeholderTextColor="gray"
             placeholder={'Enter name'}
             value={name}
